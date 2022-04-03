9b49f1f67ff75bb94ddce3b27372b5aa98f39b03
Sushant Kumar <krsushant.sk@gmail.com>
2022-04-03 11:09:20 +0530

    Improved login page design

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,33 +7,21 @@ const Login = () => {
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
-            <img 
-            src="./Sikayat2.png"
-            className="img-fluid"
-            alt ="brand logo"
-            />
-            <img
-              src="./logohome.webp"
-              className="img-fluid"
-              alt="Sample"
-            />
+            <img src="./Sikayat2.png" className="img-fluid" alt="brand logo" />
+            <img src="./logohome.webp" className="img-fluid" alt="Sample" />
           </div>
-          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <div className="text-center text-lg-start mt-4 pt-2">
-              <button
-                type="button"
-                className="btn btn-light"
-                style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-              >
-                <img src="/google_login.svg" alt = "Login Button" height={32} width={32} className="mx-2"/>
-                Login with Google
-              </button>
-            </div>
+          <div className="col-md-8 col-lg-6 col-xl-4 ">
+            <button type="button" className="google-button">
+              <span className="google-button__icon"><img src="./logo.svg" alt="signIn Logo"></img></span>
+              <span className="google-button__text">Sign in with Google</span>
+            </button>
           </div>
         </div>
       </div>
-      <div className="d-flex flex-column flex-md-column text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-dark">
-        <div className="text-white text-center mb-3 mb-md-0">IIT BBS | Sikayat Portal</div>
+      <div className="d-flex flex-column flex-md-column text-center text-md-start justify-content-between py-4 px-4 px-xl-1 bg-dark">
+        <div className="text-white text-center mb-3 mb-md-0">
+          IIT BBS | Sikayat Portal
+        </div>
       </div>
     </section>
   );
