10af8ec72b05de46823407550aafe1301cb706d8
Harsh Singh Jadon <intelligent24harsh@gmail.com>
2022-04-09 16:42:11 +0530

    Implemented Logout Backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/googlelogin', authController.googleLogin);
+router.get('/logout', authController.logout);
 
 router.use(authController.protect);
 
