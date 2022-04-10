378a3a31cbc9c7410f30c5595e73631c46119a78
Harsh Singh Jadon <intelligent24harsh@gmail.com>
2022-04-10 14:46:20 +0530

    Added Status Controller API

--- server/controllers/statusController.js
+++ server/controllers/statusController.js
@@ -10,29 +10,81 @@ const router = express.Router({ mergeParams: true });
 // 1) Check if the user is actually allowed to perform the operation on the post
 
 exports.updateStatus = catchAsync(async (req, res, next) => {
-  console.log(req.params.postId);
   const postToBeUpdated = await Post.findById(req.params.postId);
 
   // Check if the post belongs to user or not
-  console.log(req.user._id, postToBeUpdated.user._id);
   const headerUserId = String(req.user._id);
   const askingUserId = String(postToBeUpdated.user._id);
 
   if (headerUserId !== askingUserId) {
     return next(
       new AppError(
-        'Unauthorized. You are not allowed to delete this post!',
+        'Unauthorized. You are not allowed to update the status of this post!',
         401
       )
     );
   }
 
   // 2) Check the rights of the user - user or admin
-  console.log('here');
   const getUser = await User.findById(req.user._id);
-  console.log(getUser);
+  //   console.log(getUser.role, postToBeUpdated.status);
 
   // 3) If user, then go to 2
+  if (getUser.role === 'user') {
+    const doc = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        status: 2
+      },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!doc) {
+      return next(
+        new AppError('No post found with that ID for updating status', 404)
+      );
+    }
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  }
 
   // 4) If admin, then go to 1
+  if (getUser.role === 'admin') {
+    const doc = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        status: 1
+      },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!doc) {
+      return next(
+        new AppError('No post found with that ID for updating status', 404)
+      );
+    }
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  }
+
+  return next(
+    new AppError(
+      'Unauthorized. You are not allowed to update the status of this post!',
+      401
+    )
+  );
 });
