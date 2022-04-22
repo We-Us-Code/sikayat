b1694fe71ba7acdc5f508443fb10b9d585d946ad
Sushant Kumar <krsushant.sk@gmail.com>
2022-04-22 16:36:05 +0530

    Changed pointer of github to our organisation

--- client/src/components/BottomBar.js
+++ client/src/components/BottomBar.js
@@ -5,7 +5,7 @@ const BottomBar = () => {
     <div className="d-flex flex-column flex-md-column text-center text-md-start justify-content-between py-4 px-4 px-xl-1 bg-dark" style={{ marginTop: "20px" }}>
       <div className="text-white text-center mb-3 mb-md-0">
         IIT BBS 
-        <a className="align-items-center mx-3" target="_blank" rel="noreferrer" href="https://github.com/harshsingh-24/cmp" style={{ color:"inherit", textDecoration:"inherit" }}>
+        <a className="align-items-center mx-3" target="_blank" rel="noreferrer" href="https://github.com/We-Us-Code/sikayat" style={{ color:"inherit", textDecoration:"inherit" }}>
           <i className="bi bi-github" />
         </a> 
         Sikayat Portal
